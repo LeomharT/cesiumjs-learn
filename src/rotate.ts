1a1a01798c89690815ae6c9f9a92e2b14c2aa84e
LeomharT <leo.liao.lzy@gmail.com>
2025-12-19 16:47:51 +0800

    feat: step by step

--- src/rotate.ts
+++ src/rotate.ts
@@ -2,39 +2,36 @@ import "./rotate.css";
 
 const root = document.querySelector("#root") as HTMLDivElement;
 
-const box = document.createElement("div");
-box.innerText = "👉";
-box.classList.add("box");
-root.append(box);
+const COUNT = 50;
+const RADIUS = 300;
 
-let enable = false;
+for (let i = 0; i < COUNT; i++) {
+  const rect = document.createElement("div");
+  rect.classList.add("rect");
 
-root.addEventListener("pointerdown", () => {
-  enable = true;
-});
+  const x = Math.cos(i) * RADIUS;
+  const y = Math.sin(i) * RADIUS;
 
-root.addEventListener("pointerup", () => {
-  enable = false;
-});
+  rect.style.left = x + window.innerWidth / 2 + "px";
+  rect.style.top = y + window.innerHeight / 2 + "px";
 
-function onPointerMove(e: PointerEvent) {
-  if (!enable) return;
-
-  const rect = box.getBoundingClientRect();
-
-  const center = {
-    x: rect.left + rect.width / 2,
-    y: rect.top + rect.height / 2,
-  };
-
-  const target = {
-    x: e.clientX,
-    y: e.clientY,
-  };
-
-  const angle = Math.atan2(target.y - center.y, target.x - center.x);
-
-  box.style.transform = `rotate(${(angle * 180) / Math.PI}deg)`;
+  root.append(rect);
 }
 
-root.addEventListener("pointermove", onPointerMove);
+const nodes = document.querySelectorAll(".rect");
+
+function render(time: number = 0) {
+  nodes.forEach((value, index) => {
+    const x = Math.cos(index + time * 0.01) * RADIUS;
+    const y = Math.sin(index + time * 0.01) * RADIUS;
+
+    if (value instanceof HTMLDivElement) {
+      value.style.left = x + window.innerWidth / 2 + "px";
+      value.style.top = -y + window.innerHeight / 2 + "px";
+    }
+  });
+
+  requestAnimationFrame(render);
+}
+
+render();
