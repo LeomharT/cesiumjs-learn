85511a19a80374710d4078fc5b2733513a2d4883
LeomharT <leo.liao.lzy@gmail.com>
2025-12-19 17:34:41 +0800

    feat: circle

--- src/canvas.ts
+++ src/canvas.ts
@@ -1,5 +1,3 @@
-import * as EssentialsPlugin from "@tweakpane/plugin-essentials";
-import { Pane } from "tweakpane";
 import "./canvas.css";
 
 type Position = {
@@ -21,142 +19,111 @@ const sizes = {
   pixelRatio: Math.min(2.0, window.devicePixelRatio),
 };
 
-const box = {
-  count: 30,
-  hx: 50,
-  hy: 50,
-};
-
-const cursor = {
-  x: 0,
-  y: 0,
-};
-
-let accelerationX = 0;
-let translateX = 0;
-
-let accelerationY = 0;
-let translateY = 0;
-
-const positions = Array.from({ length: box.count }, () => ({
-  x: 0,
-  y: 0,
-}));
-
-/**
- * Document Object Module
- */
-
-const el = document.querySelector("#root") as HTMLDivElement;
-
-const canvas = document.createElement("canvas");
-
+const canvas: HTMLCanvasElement = document.createElement("canvas");
 canvas.width = sizes.width * sizes.pixelRatio;
 canvas.height = sizes.height * sizes.pixelRatio;
-
 canvas.style.width = sizes.width + "px";
 canvas.style.height = sizes.height + "px";
 
+const el = document.querySelector("#root");
 el?.append(canvas);
 
 const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 ctx.scale(sizes.pixelRatio, sizes.pixelRatio);
 
+const pointer = {
+  x: 0,
+  y: 0,
+};
+
+let radius = 50;
+let acceleration = false;
+
 function cleanScene() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.save();
   ctx.fillStyle = "#1e1e1e";
-  ctx.fillRect(0, 0, sizes.width, sizes.height);
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.restore();
 }
 
-function drawBox(position: Position, size: Size, time: number) {
-  const fixed = {
-    x: position.x - size.width / 2.0,
-    y: position.y - size.height / 2.0,
-  };
+function rotateAroundCursor(time: number, radius: number = 50) {
+  time *= 0.001;
 
   ctx.save();
+  ctx.fillStyle = "#36cfc9";
 
-  ctx.fillStyle = "#F9DFDF";
-  ctx.fillRect(fixed.x, fixed.y, size.width, size.height);
-
-  ctx.strokeStyle = "#001BB7";
-  ctx.setLineDash([10, 5]);
-  ctx.lineDashOffset = -time * 0.05;
+  ctx.setLineDash([5, 5]);
+  ctx.strokeStyle = "#d3adf7";
   ctx.lineWidth = 5;
-  ctx.strokeRect(fixed.x, fixed.y, size.width, size.height);
+
+  for (let i = 0; i < 50; i++) {
+    const position = {
+      x: Math.cos(i + time) * radius,
+      y: Math.sin(i + time) * radius,
+    };
+
+    const size = radius / 4;
+
+    ctx.fillRect(
+      pointer.x - size / 2 + position.x,
+      pointer.y - size / 2 + position.y,
+      size,
+      size
+    );
+    ctx.strokeRect(
+      pointer.x - size / 2 + position.x,
+      pointer.y - size / 2 + position.y,
+      size,
+      size
+    );
+  }
 
   ctx.restore();
 }
 
-/**
- * Debug
- */
-
-const pane = new Pane({ title: "Debug Params" });
-pane.element.parentElement!.style.width = "380px";
-pane.registerPlugin(EssentialsPlugin);
-
-const fpsGraph: any = pane.addBlade({
-  view: "fpsgraph",
-  label: undefined,
-  rows: 4,
-});
-
-/**
- * Event
- */
-
 function render(time: number = 0) {
-  fpsGraph.begin();
-
-  // Update
-  accelerationY += (cursor.y - translateY) * 0.5;
-  accelerationY *= 0.15;
-  translateY += accelerationY;
-
-  accelerationX += (cursor.x - translateX) * 0.5;
-  accelerationX *= 0.15;
-  translateX += accelerationX;
-
-  positions.pop();
-  positions.unshift({
-    x: translateX,
-    y: translateY,
-  });
-
-  // Render
   cleanScene();
 
-  for (let i = 0; i < box.count; i++) {
-    drawBox(
-      positions[positions.length - 1 - i],
-      { width: box.hx, height: box.hy },
-      time
-    );
+  if (acceleration) {
+    radius += 15.0;
+  } else {
+    radius -= 15.0;
   }
+
+  radius = Math.max(50, radius);
+  radius = Math.min(300, radius);
+
+  console.log(radius);
+
+  rotateAroundCursor(time, radius);
 
   // Animation
   requestAnimationFrame(render);
-
-  fpsGraph.end();
 }
+
 render();
 
-function resize() {
+window.addEventListener("resize", () => {
   sizes.width = window.innerWidth;
   sizes.height = window.innerHeight;
 
   canvas.width = sizes.width * sizes.pixelRatio;
   canvas.height = sizes.height * sizes.pixelRatio;
-
   canvas.style.width = sizes.width + "px";
   canvas.style.height = sizes.height + "px";
 
-  ctx.scale(sizes.pixelRatio, sizes.pixelRatio);
-}
-window.addEventListener("resize", resize);
+  cleanScene();
+});
 
-function onPointerMove(e: PointerEvent) {
-  cursor.x = e.clientX;
-  cursor.y = e.clientY;
-}
-el.addEventListener("pointermove", onPointerMove);
+window.addEventListener("pointermove", (e) => {
+  pointer.x = e.clientX;
+  pointer.y = e.clientY;
+});
+
+window.addEventListener("pointerdown", () => {
+  acceleration = true;
+});
+window.addEventListener("pointerup", () => {
+  acceleration = false;
+});
