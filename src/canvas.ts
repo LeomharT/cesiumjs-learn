7a4c5858e0cf77667d3f6791fd1dbeb29ea2c77d
LeomharT <leo.liao.lzy@gmail.com>
2026-02-27 10:40:30 +0800

    feat: rotate follow cursor

--- src/canvas.ts
+++ src/canvas.ts
@@ -1,159 +1,102 @@
 import "./canvas.css";
-import { fs, vs } from "./penger";
 
-type Vector2 = {
-  x: number;
-  y: number;
-};
-
-type Vector3 = Vector2 & {
-  z: number;
-};
-
-const font = new FontFace(
-  "Alimama_ShuHeiTi_Bold",
-  "url(Alimama_ShuHeiTi_Bold.ttf)",
-  {
-    style: "italic",
-    weight: "400",
-    stretch: "condensed",
-  },
-);
-document.fonts.add(font);
-await font.load();
-
-const sizes = {
+const size = {
   width: window.innerWidth,
   height: window.innerHeight,
-  aspect: window.innerWidth / window.innerHeight,
 };
+const el = document.querySelector("#root");
+
+let isHoding = false;
+
+function random(min: number, max: number) {
+  return Math.random() * (max - min) + min;
+}
+
+const positions = Array.from({ length: 50 }, () => ({
+  x: random(0, size.width),
+  y: random(0, size.height),
+  angle: random(0, Math.PI * 2),
+  length: random(50, 150),
+}));
+
+const colors = [
+  "#165A36",
+  "#1C6E42",
+  "#238551",
+  "#32A467",
+  "#72CA9B",
+  "#5642A6",
+  "#FFE39F",
+  "#ABC4A2",
+  "#6B9FA1",
+  "#3E769E",
+  "#1F4B99",
+];
 
 const canvas = document.createElement("canvas");
-canvas.width = sizes.width;
-canvas.height = sizes.height;
-canvas.style.widows = sizes.width + "px";
-canvas.style.height = sizes.height + "px";
-
-const el = document.querySelector("#root");
+canvas.width = size.width;
+canvas.height = size.height;
+canvas.style.width = size.width + "px";
+canvas.style.height = size.height + "px";
 el?.append(canvas);
 
 const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 
 function clear() {
   ctx.save();
-  ctx.fillStyle = "#1e1e1e";
-  ctx.fillRect(0, 0, sizes.width, sizes.height);
+  ctx.fillStyle = "#111418";
+  ctx.fillRect(0, 0, size.width, size.height);
   ctx.restore();
 }
-clear();
 
-function point(v: Vector2) {
+function renderGrassBlade() {
   ctx.save();
-  const s = 10;
-  ctx.fillStyle = "#A8DF8E";
-  ctx.fillRect(v.x - s / 2, v.y - s / 2, s, s);
-  ctx.restore();
-}
+  ctx.lineWidth = 4;
 
-function screen(v: Vector2) {
-  const x = ((v.x + 1.0) / 2.0) * sizes.width;
-  const y = -((v.y - 1.0) / 2.0) * sizes.height;
+  for (let i = 0; i < positions.length; i++) {
+    const pos = positions[i];
 
-  return {
-    x,
-    y,
-  };
-}
+    ctx.save();
 
-function project(v: Vector3) {
-  return {
-    x: v.x / v.z,
-    y: (v.y / v.z) * sizes.aspect,
-  };
-}
+    ctx.translate(pos.x, pos.y);
+    ctx.rotate(pos.angle);
 
-function translateZ(v: Vector3, dz: number) {
-  return {
-    ...v,
-    z: v.z + dz,
-  };
-}
+    ctx.beginPath();
+    ctx.moveTo(-pos.length / 2, 0);
+    ctx.lineTo(pos.length / 2, 0);
 
-function line(from: Vector2, to: Vector2) {
-  ctx.save();
+    ctx.strokeStyle = colors[i % colors.length];
+    ctx.stroke();
 
-  ctx.beginPath();
-  ctx.moveTo(from.x, from.y);
-  ctx.lineTo(to.x, to.y);
-  ctx.lineWidth = 2;
-  ctx.strokeStyle = "#A8DF8E";
-  ctx.stroke();
+    ctx.restore();
+  }
 
   ctx.restore();
 }
 
-function rotate(v: Vector3, angle: number) {
-  const x = v.x * Math.cos(angle) - v.z * Math.sin(angle);
-  const z = v.x * Math.sin(angle) + v.z * Math.cos(angle);
+canvas.addEventListener("pointerdown", () => (isHoding = true));
+canvas.addEventListener("pointerup", () => (isHoding = false));
+canvas.addEventListener("pointermove", (e) => {
+  if (!isHoding) return;
 
-  return {
-    x,
-    y: v.y,
-    z,
+  const cursor = {
+    x: e.clientX,
+    y: e.clientY,
   };
-}
 
-const _vs = vs ?? [
-  { x: 0.25, y: 0.25, z: 0.25 },
-  { x: -0.25, y: 0.25, z: 0.25 },
-  { x: -0.25, y: -0.25, z: 0.25 },
-  { x: 0.25, y: -0.25, z: 0.25 },
+  for (let i = 0; i < positions.length; i++) {
+    const center = positions[i];
 
-  { x: 0.25, y: 0.25, z: -0.25 },
-  { x: -0.25, y: 0.25, z: -0.25 },
-  { x: -0.25, y: -0.25, z: -0.25 },
-  { x: 0.25, y: -0.25, z: -0.25 },
-];
+    const angle = Math.atan2(center.y - cursor.y, center.x - cursor.x);
 
-const _fs = fs ?? [
-  [0, 1, 2, 3],
-  [4, 5, 6, 7],
-  [0, 4],
-  [1, 5],
-  [2, 6],
-  [3, 7],
-];
+    positions[i].angle = angle + Math.PI / 2;
+  }
+});
 
-let prevTime = 0.0;
-let dz = 1.5;
-let angle = 0.0;
-
-function render(time: number = 0) {
+function render() {
+  // Clean scene
   clear();
-
-  // Delta
-  const dt = (time - prevTime) / 1000;
-  prevTime = time;
-
-  // dz += dt;
-  angle += dt;
-
-  // Render
-  for (const v of _vs) {
-    // point(screen(project(translateZ(rotate(v, angle), dz))));
-  }
-
-  for (const f of _fs) {
-    for (let i = 0; i < f.length; i++) {
-      const a = _vs[f[i]];
-      const b = _vs[f[(i + 1) % f.length]];
-
-      line(
-        screen(project(translateZ(rotate(a, angle), dz))),
-        screen(project(translateZ(rotate(b, angle), dz))),
-      );
-    }
-  }
+  renderGrassBlade();
 
   // Animation
   requestAnimationFrame(render);
